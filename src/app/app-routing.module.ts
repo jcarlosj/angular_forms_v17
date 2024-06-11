1077d991dd34f58fa9636b4fed8520094d54c047
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2024-06-11 15:00:33 -0500

    [+] app-routing.module.ts: Define ruta para componente ReactiveFormComponent [-] reactive-form.component.spec.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ReactiveFormComponent } from './topics/reactive-form/reactive-form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'reactive-form', component: ReactiveFormComponent },
+  { path:'**', redirectTo: 'reactive-form'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+ 
